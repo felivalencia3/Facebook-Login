995671cc183fd2e7a711a80881831352056f9ac6
Felipe Valencia <fvalencia@vc-soft.com>
2019-04-29 18:27:07 -0500

    Finished Mini Login App

--- src/main/java/valencia/spring/facebooklogin/FacebookLoginApplication.java
+++ src/main/java/valencia/spring/facebooklogin/FacebookLoginApplication.java
@@ -3,13 +3,39 @@ package valencia.spring.facebooklogin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class FacebookLoginApplication {
-
+@RestController
+public class FacebookLoginApplication extends WebSecurityConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(FacebookLoginApplication.class, args);
     }
 
+    @RequestMapping(value = "/user")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**", "/webjars/**", "/error**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .permitAll().and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
 }
